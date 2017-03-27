b98b145a2670422fa5ed717c63f7e66bb44297c8
Shabeer M <msg2shabeer@gmail.com>
2017-03-27 22:12:39 +0530

    [edit] order complaints in Object Oriented way, and also a simple add new complaint functionality

--- js/resolver.js
+++ js/resolver.js
@@ -1,48 +1,30 @@
-// Complaint view model
-function ComplaintsViewModel() {
-	var self = this;
-	self.complaintsURI = 'http://localhost:5000/complaints/';
-	self.complaints = ko.observableArray();
-
-	self.ajax = function(uri, method, data) {
-		var request = {
-			url: uri,
-			type: method,
-			contentType: "application/json",
-			accepts: "application/json",
-			cache: false,
-			dataType: 'jsonp',
-			data: JSON.stringify(data),
-			beforeSend: function (xhr) {
-			},
-			error: function(jqXHR) {
-				console.log("ajax error " + jqXHR.status);
-			}
-		};
-		return $.ajax(request);
-	}
-
-	self.beginAdd = function() {
-		// alert("Add");
-		$("#addComplaintModel").show();
-	}
-
-	self.markDone = function(task) {
-		task.done(true);
-	}
-
-	self.ajax(self.complaintsURI, 'GET').done(function(data) {
-		for (var i = 0; i < data.complaints.length; i++) {
-			self.complaints.push({
-				service: ko.observable(data.complaints[i].service),
-				complaint_type: ko.observable(data.complaints[i].complaint_type),
-				cust_name: ko.observable(data.complaints[i].cust_name),
-				complaint_phone:ko.observable(data.complaints[i].complaint_phone),
-				cust_address:ko.observable(data.complaints[i].cust_address),
-				date_time:ko.observable(data.complaints[i].date_time),
-			});
-		}
-	});
+// Complaint Model
+var Complaint = function (cust_id,cust_name,cust_address,cust_phone) {
+	this.cust_id			=	cust_id;
+	this.cust_name			=	cust_name;
+	this.cust_address		=	cust_address;
+	this.cust_phone			=	cust_phone;
 }
 
-ko.applyBindings(new ComplaintsViewModel(), $('#main')[0]);
+// Complaint view model
+var ComplaintsViewModel = function(){
+	this.cust_id			=	ko.observable();
+	this.cust_name			=	ko.observable();
+	this.cust_address		=	ko.observable();
+	this.cust_phone			=	ko.observable();
+	
+	this.complaints = ko.observableArray([
+	]);
+
+	this.addComplaint = function () {
+        if ((this.cust_id() != "") && (this.cust_name() != "") && (this.cust_address() != "") && (this.cust_phone() != ""))
+            this.complaints.push(new Complaint(this.cust_id(),this.cust_name(),this.cust_address(),this.cust_phone()));
+        this.cust_id(""); // Clear the text box
+        this.cust_name("");
+        this.cust_address("");
+        this.cust_phone("");
+        $('#addComplaintModel').hide();
+    };
+};
+
+ko.applyBindings(new ComplaintsViewModel());
